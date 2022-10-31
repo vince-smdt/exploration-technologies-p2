2a8f13f34f699b0bedceac6c86eb6c62ed86afec
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2022-10-31 12:10:28 -0400

    Create client side

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-toolbar-title>Démonstration avec Heroku</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      //
+    }),
+  }
+</script>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    
+  </v-container>
+</template>
+
+<script>
+  //import API from '@/api';
+  export default {
+    name: 'MyHome',
+  }
+</script>
